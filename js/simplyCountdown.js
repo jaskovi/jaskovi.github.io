ee63790d435605615271dff45bf172a1729c3b53
Katerina Brejchova <brejchovakaterina97@gmail.com>
2022-03-26 17:13:16 +0100

    Footer size, sections, counter

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -122,11 +122,11 @@
                 minutes: 0,
                 seconds: 0,
                 words: {
-                    days: 'day',
-                    hours: 'hour',
-                    minutes: 'minute',
-                    seconds: 'second',
-                    pluralLetter: 's'
+                    days: 'dnů',
+                    hours: 'hodin',
+                    minutes: 'minut',
+                    seconds: 'sekund',
+                    pluralLetter: ''
                 },
                 plural: true,
                 inline: false,
